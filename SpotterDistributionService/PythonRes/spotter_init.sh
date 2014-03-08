5052dfbc928751768ecb67b219f431763ed18509
belaa007 <belaa007@hotmail.com>
2014-03-08 16:39:43 +0100

    Bash initscript add

---
+++ SpotterDistributionService/PythonRes/spotter_init.sh
@@ -0,0 +1,28 @@
+﻿#!/bin/bash
+
+#fping install
+sudo yum -y --nogpgcheck install fping
+
+#get-pip download
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+
+#pip install
+sudo python get-pip.py
+
+#bottle install
+sudo pip install bottle
+
+#cherrypy install
+sudo pip install cherrypy
+
+#download spotter
+wget https://raw.github.com/belaa007/Spotter/master/SpotterDistributionService/PythonRes/spotter.py
+
+#get hostname
+hostname > config
+
+#get ip
+curl http://checkip.dyndns.org | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' >> config
+
+#start spotter
+python spotter.py
